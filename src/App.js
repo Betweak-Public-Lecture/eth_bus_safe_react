83d1661da96c2cb9fb6b93d9d8643cbace80e75d
BeTweak <ys@betweak.com>
2020-12-17 18:08:45 +0900

    2

--- src/App.js
+++ src/App.js
@@ -8,11 +8,16 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
-// Car Crud Page
+// Car CRUD Page
 import CarList from './pages/Car/CarList';
 import CarDetail from './pages/Car/CarDetail';
 import CarEdit from './pages/Car/CarDetail';
 import CarAdd from './pages/Car/CarAdd';
+
+// CheckList Pages
+import CheckList from './pages/CheckList';
+import checkListPost from './pages/CheckListPost';
+import CheckListPost from './pages/CheckListPost';
 
 function App() {
   const [isLogined, setIsLogined] = React.useState(false);
@@ -48,6 +53,16 @@ function App() {
         <Route path='/' exact component={Home} />
         <Route path='/login' exact component={(prop) => <Login {...prop} setAuthInfo={(authInfo)=>setAuthInfo(authInfo)} />}  />
         <Route path='/register' exact component={Register} />
+
+        {/* Car Routes */}
+        <Route path='/carlist' exact component={CarList} />
+        <Route path='/caradd' exact component={CarAdd} />
+        <Route path='/car/:carId/edit' exact component={CarEdit} />
+        <Route path='/car/:carId' exact component={CarDetail} />
+
+        {/* CheckList Routes */}
+        <Route path='/checklist' exact component={CheckList} />
+        <Route path='/checklist/:carId/post' exact component={CheckListPost} />
       </Switch>
 
     </Router>
---
+++ src/pages/CheckListPost.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap';
+
+export default function CheckListPost({history, match}) {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>체크리스트</h1>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
